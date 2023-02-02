b1022605613888adc0bef818f271b24c8590d20e
Gabriel <caampos22@gmail.com>
2023-02-01 23:49:53 -0300

    :tada: first commit

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/theme';
+import { Home } from './src/pages';
+
+function App(): JSX.Element {
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+      <ThemeProvider theme={theme}>
+        <Home />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['./src'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@storage': './src/storage',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
+};
